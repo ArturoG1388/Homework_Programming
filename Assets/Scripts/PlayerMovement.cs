a197cc75dfa490dae89b334a044dbf80c41c4f8a
ArturoG1388 <arturo.garcia092708@gmail.com>
2025-02-22 14:05:47 -0500

    Shooter

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,5 +2,27 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D _Rigidbody2D;
+
+    
+
+    private float xSpeed;
+    private float ySpeed;
+
+    public float speed = 3;
+
+    private string InputX = "Horizontal";
+    private string InputY = "Vertical";
+    private void Start()
+    {
+        _Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        xSpeed = Input.GetAxis(InputX);
+        ySpeed = Input.GetAxis(InputY);
+
+        _Rigidbody2D.velocity = new Vector2(xSpeed, ySpeed) * speed;
+    }
 
 }
--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -2,5 +2,71 @@ using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour
 {
+    public GameObject PreFab;
+    public GameObject SpawnFab;
+    public Transform bulletTrash;
+    public Transform bulletSpawn;
+
+    private const float Timer = 0.5f;
+    private float _currentTime = 0.5f;
+    private bool _canshoot = true;
+    private void Update()
+    {
+        TimerMethod();
+        Shoot();
+
+
+
+
+       
+    }
+    private void TimerMethod()
+    {
+        if (!_canshoot)
+        {
+            _currentTime -= Time.deltaTime;
+
+            if (_currentTime > 0)
+            {
+                _canshoot = true;
+                _currentTime = Timer;
+            }
+        }
+    }
+
+    private void Shoot()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && _canshoot)
+        {
+            GameObject bullet = Instantiate(PreFab, bulletSpawn.position, Quaternion.identity);
+
+            bullet.transform.SetParent(bulletTrash);
+
+            _canshoot = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse1) && _canshoot)
+        {
+            GameObject bullet = Instantiate(SpawnFab, bulletSpawn.position, Quaternion.identity);
+
+            bullet.transform.SetParent(bulletTrash);
+
+            _canshoot = false;
+        }
+
+    }
+
+
+
+
+
 
 }
+
+    
+    
+        
+    
+
+
+
+
